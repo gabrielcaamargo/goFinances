f0e6c390de96b9b6d125f805b586eb38038c6e5b
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-07-29 20:52:30 -0300

    feat: set linters

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{
+      flex: 1, alignItems: 'center', justifyContent: 'center'
+    }}>
+      <Text>App</Text>
+    </View>
+  );
+}

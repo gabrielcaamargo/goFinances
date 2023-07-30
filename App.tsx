5ad970d528a7a53fdd7bd6648707f52df03afadc
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-07-29 21:54:48 -0300

    feat: set custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import Dashboard from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
+
+import {
+  useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return (
+      <View style={{
+        flex: 1, alignItems: 'center', justifyContent: 'center'
+      }}>
+        <ActivityIndicator size={64} />
+      </View>
+    );
+  }
+
   return (
-    <View style={{
-      flex: 1, alignItems: 'center', justifyContent: 'center'
-    }}>
-      <Text>App</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from './styles';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
